fc5aed91ae71aa3a824652ec9493632df2bd1c47
victormusembi11 <victor.musembi11@gmail.com>
2023-12-04 18:34:13 +0300

    refactor: split settings file btn prod and local

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from config.utils import get_bool_env, get_env_variable
+from config.settings.utils import get_bool_env, get_env_variable
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = get_bool_env("DEBUG")
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -70,17 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
---
+++ config/settings/prod.py
@@ -0,0 +1,16 @@
+from config.settings.base import *
+
+ALLOWED_HOSTS = [
+    ".localhost",
+    "127.0.0.1",
+]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
